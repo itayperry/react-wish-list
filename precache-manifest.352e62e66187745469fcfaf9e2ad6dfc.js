9050009e3c89810c778d634b8feaa63f9cd270df
itay <itayperry91@gmail.com>
2019-06-26 12:57:30 +0300

    Updates

---
+++ precache-manifest.352e62e66187745469fcfaf9e2ad6dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb757203dc5c5136920070cd1973c98",
+    "url": "/react-wish-list/index.html"
+  },
+  {
+    "revision": "fab4e50a122f3bb9879d",
+    "url": "/react-wish-list/static/css/main.4e6223e0.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/react-wish-list/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "fab4e50a122f3bb9879d",
+    "url": "/react-wish-list/static/js/main.e6a577c9.chunk.js"
+  },
+  {
+    "revision": "1d6b7dbb425c228becf9",
+    "url": "/react-wish-list/static/js/runtime~main.2009c35e.js"
+  }
+]);
